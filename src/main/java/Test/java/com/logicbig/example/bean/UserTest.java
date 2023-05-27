568abdb05d1459ed2517f126193a3721136ed95c
XimenaRodriguez20 <123812926+ximenarodriguez20@users.noreply.github.com>
2023-05-26 21:36:20 -0500

    pom actualizada, prueba
    Co-authored-by: MPulidoM <118181543+mpulidom@users.noreply.github.com>
    Co-authored-by: JordyBautista10 <123812969+jordybautista10@users.noreply.github.com>
    Co-authored-by: Erika Juliana Castro Romero <118181224+juc28@users.noreply.github.com>
    Co-authored-by: SantiagoMelo0104 <123812833+santiagomelo0104@users.noreply.github.com>

--- src/main/java/Test/java/com/logicbig/example/bean/UserTest.java
+++ src/main/java/Test/java/com/logicbig/example/bean/UserTest.java
@@ -37,39 +37,10 @@ public class UserTest {
     private Users user;
     private String username;
 
-    @BeforeAll
-    public void setup() {
-        // this starts only the first time of we execute with class test
-        MockitoAnnotations.initMocks(this);
-        user = new Users("Mpulido21","lhanna.23","Mariana Pulido","Proponente","Estudiante",20,"Matematicas","mpm@gmail.com");
-
-        username = user.getUsername();
-        System.out.println(user);
-        when(userService.getUser(username)).thenReturn(user);
-        Mocks mocks = new Mocks();
-        when(facesContextWrapper.getCurrentInstance()).thenReturn(mocks.facesContextMock);
-        when(primeFacesWrapper.current()).thenReturn(mocks.primeFaces);
-    }
-
     @Test
-    public void checkIfTheUserAdds() {
-        user = new Users("Mpulido21","lhanna.23","Mariana Pulido","Proponente","Estudiante",20,"Matematicas","mpm@gmail.com");
-        System.out.println(user);
-
-        String username = "Mpulido21";
-        String password = "lhanna.23";
-        String personalName = "Mariana Pulido";
-        String profile = "Proponente";
-        String rol = "Estudiante";
-        int age = 20;
-        String area = "Matematicas";
-        String email = "mpm@gmail.com";
-        userBean.addUser();
-        System.out.println(userService.getAllUsers());
-        boolean addUser = userService.userExist("Mpulido21");
-        System.out.println(addUser);
-        System.out.println(userService.getAllUsers());
-        assertTrue(addUser);
+    public void getProfileOnString() {
+        user = new Users("Mpulido21", "lhanna.23", "Mariana Pulido", "Proponente", "Estudiante", 20, "Matematicas", "mpm@gmail.com");
+        assertEquals("Mpulido21", user.getUsername());
     }
 
 
