a95f8304a1e0f9dd7398e2f55f11e0278704791b
MpulidoM <marianapulido075@gmail.com>
2023-05-27 01:17:43 -0500

    Pruebas1Version

---
+++ src/main/java/Test/java/com/logicbig/example/bean/IdeasTest.java
@@ -0,0 +1,82 @@
+package Test.java.com.logicbig.example.bean;
+
+import com.logicbig.example.bean.IdeasBean;
+import com.logicbig.example.bean.UserBean;
+import com.logicbig.example.data.*;
+import com.logicbig.example.faces.PrimeFacesWrapper;
+import org.junit.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import javax.faces.context.FacesContextWrapper;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class IdeasTest {
+    @Mock
+    private FacesContextWrapper facesContextWrapper;
+
+    @Mock
+    private PrimeFacesWrapper primeFacesWrapper;
+
+    @Mock // Esta es la clase real
+    private IdeasService ideasService;
+    private IdeaRepository ideaRepository;
+
+    @InjectMocks // Esta es la clase real
+    private IdeasBean IdeasBean;
+
+    private Ideas ideas;
+    private String name;
+
+    @Test
+    public void getNameOnString() {
+        ideas = new Ideas("Reciclaje a 1 pasos","Ambiental","Campaña de nuevos metodos verdes","Mpulido23","estudiante","Clasificacion de residuos, ambiental, verde, ecofriendly","Sociales");
+        assertEquals("Reciclaje a 1 pasos", ideas.getName());
+    }
+    @Test
+    public void getStateOnString() {
+        ideas = new Ideas("Reciclaje a 1 pasos","Ambiental","Campaña de nuevos metodos verdes","Mpulido23","estudiante","Clasificacion de residuos, ambiental, verde, ecofriendly","Sociales");
+        assertEquals("Pendiente", ideas.getEstado());
+    }
+
+    @Test
+    public void  getProponentOnString() {
+        ideas = new Ideas("Reciclaje a 1 pasos","Ambiental","Campaña de nuevos metodos verdes","Mpulido23","estudiante","Clasificacion de residuos, ambiental, verde, ecofriendly","Sociales");
+        assertEquals("Mpulido23", ideas.getProponent());
+    }
+
+    @Test
+    public void  getKeyWordsOnString() {
+        ideas = new Ideas("Reciclaje a 1 pasos","Ambiental","Campaña de nuevos metodos verdes","Mpulido23","estudiante","Clasificacion de residuos, ambiental, verde, ecofriendly","Sociales");
+        assertEquals("Clasificacion de residuos, ambiental, verde, ecofriendly", ideas.getKeyWords());
+    }
+
+    @Test
+    public void  getlikesCount() {
+        ideas = new Ideas("Reciclaje a 1 pasos","Ambiental","Campaña de nuevos metodos verdes","Mpulido23","estudiante","Clasificacion de residuos, ambiental, verde, ecofriendly","Sociales");
+        assertEquals(0, ideas.getLikesCount());
+    }
+
+    @Test
+    public void  getTopicAndArea() {
+        ideas = new Ideas("Reciclaje a 1 pasos","Ambiental","Campaña de nuevos metodos verdes","Mpulido23","estudiante","Clasificacion de residuos, ambiental, verde, ecofriendly","Sociales");
+        assertEquals("Sociales", ideas.getProponentArea());
+        assertEquals("Ambiental", ideas.getTopic());
+    }
+
+    @Test
+    public void NotanExistsIdea() {
+        when(ideasService.getIdea(name)).thenReturn(null);
+        ideas = new Ideas("Reciclaje a 1 pasos","Ambiental","Campaña de nuevos metodos verdes","Mpulido23","estudiante","Clasificacion de residuos, ambiental, verde, ecofriendly","Sociales");
+        ideasService.addIdeas(ideas);
+        Ideas ideas1 = ideasService.getIdea("Reciclaje a 2 pasos");
+        assertEquals( null, ideas1);
+    }
+}
--- src/main/java/Test/java/com/logicbig/example/bean/UserTest.java
+++ src/main/java/Test/java/com/logicbig/example/bean/UserTest.java
@@ -1,6 +1,8 @@
 package Test.java.com.logicbig.example.bean;
 import com.logicbig.example.bean.UserBean;
+import com.logicbig.example.data.UserRepository;
 import com.logicbig.example.data.UserService;
+
 import com.logicbig.example.data.Users;
 import com.logicbig.example.faces.PrimeFacesWrapper;
 import org.junit.jupiter.api.BeforeAll;
@@ -30,6 +32,7 @@ public class UserTest {
 
     @Mock // Esta es la clase real
     private UserService userService;
+    private UserRepository userRepository;
 
     @InjectMocks // Esta es la clase real
     private UserBean userBean;
@@ -38,10 +41,43 @@ public class UserTest {
     private String username;
 
     @Test
-    public void getProfileOnString() {
+    public void getUsernameOnString() {
         user = new Users("Mpulido21", "lhanna.23", "Mariana Pulido", "Proponente", "Estudiante", 20, "Matematicas", "mpm@gmail.com");
         assertEquals("Mpulido21", user.getUsername());
     }
+    @Test
+    public void getProfileOnString() {
+        user = new Users("Mpulido21", "lhanna.23", "Mariana Pulido", "Proponente", "Estudiante", 20, "Matematicas", "mpm@gmail.com");
+        assertEquals("Proponente", user.getProfile());
+    }
 
+    @Test
+    public void UpdateUserUsername() {
+        user = new Users("Mpulido21", "lhanna.23", "Mariana Pulido", "Proponente", "Estudiante", 20, "Matematicas", "mpm@gmail.com");
+        user.setUsername("Mpulido5");
+        assertEquals( "Mpulido5", user.getUsername());
+    }
 
+    @Test
+    public void UpdateUserPassword() {
+        user = new Users("Mpulido21", "lhanna.23", "Mariana Pulido", "Proponente", "Estudiante", 20, "Matematicas", "mpm@gmail.com");
+        user.setPassword("junior10");
+        assertEquals( "junior10", user.getPassword());
+    }
+    @Test
+    public void AddExistsUser() {
+        when(userService.userExist("Mpulido21")).thenReturn(true);
+        user = new Users("Mpulido21", "lhanna.23", "Mariana Pulido", "Proponente", "Estudiante", 20, "Matematicas", "mpm@gmail.com");
+        userService.addUser(user);
+        boolean useradd = userService.userExist("Mpulido21");
+        assertEquals( true, useradd);
+    }
+    @Test
+    public void UserDontExists() {
+        when(userService.userExist("Mpulido2")).thenReturn(false);
+        user = new Users("Mpulido21", "lhanna.23", "Mariana Pulido", "Proponente", "Estudiante", 20, "Matematicas", "mpm@gmail.com");
+        userService.addUser(user);
+        boolean useradd = userService.userExist("Mpulido2");
+        assertEquals( false, useradd);
+    }
 }
