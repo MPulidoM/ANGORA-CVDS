4a395828c2b650b88648e778c7e3e672bc6f44ac
Jordy <jordy.sbautista@gmail.com>
2023-05-27 00:45:44 -0500

    Olores 12
    Co-authored-by: MPulidoM <118181543+mpulidom@users.noreply.github.com>
    Co-authored-by: XimenaRodriguez20 <123812926+ximenarodriguez20@users.noreply.github.com>
    Co-authored-by: SantiagoMelo0104 <123812833+santiagomelo0104@users.noreply.github.com>
    Co-authored-by: Juc28 <erika.castro.romero28@gmail.com>

--- src/main/java/com/logicbig/example/Main.java
+++ src/main/java/com/logicbig/example/Main.java
@@ -29,19 +29,23 @@ public class Main{
     @Bean(name = "database")
     public CommandLineRunner run() {
         return args -> {
+            String ADMINISTRACION = "Administracion";
+            String SISTEMAS = "Sistemas";
+            String SOCIALES = "Sociales";
+            String MATEMATICAS = "Matematicas";
             userService.addUser(new Users("AdminAngora","Ad101Angora","Carlos Leal","Administrador","Empleado",45,"Sistemas","Adminastor.Angora@gmail.com"));
             userService.addUser(new Users("Mpulido23","lhanna.23","Mariana Pulido","Proponente","Estudiante",20,"Matematicas","mpm@gmail.com"));
             userService.addUser(new Users("AmbientalINC.ADMIN","pro.ambiental","Alberto Pardo","Cliente","Gerente de Proyectos ",39,"Deporte","ProyectosPlaneta@ambientalINC.com"));
-            ideasService.addIdeas(new Ideas("Actualizacion de Software","Tecnologia","Campaña de nuevos metodos verdes","Mpulido23","estudiante","Clasificacion de residuos, ambiental, verde, ecofriendly","Sociales"));
-            ideasService.addIdeas(new Ideas("Teorema ed Karatsuba","Deportes","Campaña de nuevos metodos verdes","Ad101Angora","Trabajador","para que, no lo se, amigable","Administracion"));
-            ideasService.addIdeas(new Ideas("Procesos contables","Salud","Campaña de nuevos metodos verdes","AmbientalINC.ADMIN","Profesor","vida, salud, recuperacion","Matematicas"));
-            ideasService.addIdeas(new Ideas("Flujo de caja Positivo","Economia","Campaña de nuevos metodos verdes","Ad101Angora","Mesero","software, hardware, elementos","Sistemas"));
-            ideasService.addIdeas(new Ideas("Venta de computo","Infraestructura","Campaña de nuevos metodos verdes","Mpulido23","Filosofo","otorrinonaingologo, parangaricutirimicuaro","Sistemas"));
-            ideasService.addIdeas(new Ideas("pañales","Ambiental","Campaña de nuevos pañales","Mpulido23","Urologo","niños, pañal, rojo, ecofriendly","Administracion"));
-            ideasService.addIdeas(new Ideas("pañales2.0","Social","Campaña de nuevos pañales2.0","Mpulido23","Pediatra","llorone,niños, pañal, verde, ecofriendly","Sociales"));
-            ideasService.addIdeas(new Ideas("computadores","Tecnología","Campaña Tecnología","Mpulido23","mecanico","mundo,tecnologia","Matematicas"));
-            ideasService.addIdeas(new Ideas("Tableros","Educación","Campaña de Educación","Mpulido23","papa","niños, estudiantes","Sociales"));
-            ideasService.addIdeas(new Ideas("Muebles","Infraestructura","Campaña de nuevos muebles","Mpulido23","estudiante","muebles,nuevos, verde, ecofriendly","Administracion"));
+            ideasService.addIdeas(new Ideas("Actualizacion de Software","Tecnologia","Campaña de nuevos metodos verdes","Mpulido23","estudiante","Clasificacion de residuos, ambiental, verde, ecofriendly",SOCIALES));
+            ideasService.addIdeas(new Ideas("Teorema ed Karatsuba","Deportes","Campaña de nuevos metodos verdes","Ad101Angora","Trabajador","para que, no lo se, amigable",ADMINISTRACION));
+            ideasService.addIdeas(new Ideas("Procesos contables","Salud","Campaña de nuevos metodos verdes","AmbientalINC.ADMIN","Profesor","vida, salud, recuperacion",MATEMATICAS));
+            ideasService.addIdeas(new Ideas("Flujo de caja Positivo","Economia","Campaña de nuevos metodos verdes","Ad101Angora","Mesero","software, hardware, elementos",SISTEMAS));
+            ideasService.addIdeas(new Ideas("Venta de computo","Infraestructura","Campaña de nuevos metodos verdes","Mpulido23","Filosofo","otorrinonaingologo, parangaricutirimicuaro",SISTEMAS));
+            ideasService.addIdeas(new Ideas("pañales","Ambiental","Campaña de nuevos pañales","Mpulido23","Urologo","niños, pañal, rojo, ecofriendly",ADMINISTRACION));
+            ideasService.addIdeas(new Ideas("pañales2.0","Social","Campaña de nuevos pañales2.0","Mpulido23","Pediatra","llorone,niños, pañal, verde, ecofriendly",SOCIALES));
+            ideasService.addIdeas(new Ideas("computadores","Tecnología","Campaña Tecnología","Mpulido23","mecanico","mundo,tecnologia",MATEMATICAS));
+            ideasService.addIdeas(new Ideas("Tableros","Educación","Campaña de Educación","Mpulido23","papa","niños, estudiantes",SOCIALES));
+            ideasService.addIdeas(new Ideas("Muebles","Infraestructura","Campaña de nuevos muebles","Mpulido23","estudiante","muebles,nuevos, verde, ecofriendly",ADMINISTRACION));
         };
     }
 
